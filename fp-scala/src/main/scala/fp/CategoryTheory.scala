9085cce469079d14d7f39f02c2175411558cab0d
James Earl Douglas <james@earldouglas.com>
2012-10-24 21:11:09 -0700

    flatten project: fp-scala

---
+++ fp-scala/src/main/scala/fp/CategoryTheory.scala
@@ -0,0 +1,13 @@
+trait Functor[A, F[_]] {
+  def map[B](f: A => B): F[B]
+}
+
+trait Monad[A, F[_]] extends Functor[A, F] {
+  def flatMap[B](f: A => F[B]): F[B]
+}
+
+class Reader[E, A](g: E => A) extends Monad[A, ({type λ[α] = E => α})#λ] {
+  def map[B](f: A => B): E => B = { e => f(g(e)) }
+  def flatMap[B](f: A => (E => B)): E => B = { e => f(g(e))(e) }
+}
+
