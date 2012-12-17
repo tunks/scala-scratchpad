8468a2d87219518a0a18ed5850d9298c166b95c6
James Earl Douglas <james@earldouglas.com>
2012-12-16 17:04:52 -0800

    clean up the fn1arrow code

--- category-theory/Douglas.scala
+++ category-theory/Douglas.scala
@@ -230,68 +230,38 @@ trait StateMonadDemo2 {
 
 // Arrow
 
-trait Arrow[A[_,_]] {
-  def arr[B,C](f: B => C): A[B,C]
-  def >>>[B,C,D](f: B => C, g: C => D): A[B,D]
-  def first[B,C,D](f: B => C): A[(B,D),(C,D)]
-  def second[B,C,D](f: B => C): A[(D,B),(D,C)]
-  def ***[B,C,D,E](f: B => C, g: D => E): A[(B,D),(C,E)]
-  def &&&[B,C,D](f: B => C, g: B => D): A[B,(C,D)]
+trait Arrow[A, B, F[_,_]] {
+  def arr: F[A,B]
+  def >>>[C](f: F[B,C]): F[A,C]
+  def first[C]: F[(A,C),(B,C)]
+  def second[C]: F[(C,A),(C,B)]
+  def ***[C,D](f: F[C,D]): F[(A,C),(B,D)]
+  def &&&[C](f: F[A,C]): F[A,(B,C)]
 }
 
-class Fn1Arrow extends Arrow[Function1] {
-  override def arr[B,C](f: B => C): B => C = f
-  override def >>>[B,C,D](f: B => C, g: C => D): B => D = b => g(f(b))
-  override def first[B,C,D](f: B => C): ((B,D)) => (C,D) = bd => (f(bd._1), bd._2)
-  override def second[B,C,D](f: B => C): ((D,B)) => (D,C) = db => first(f)(db.swap).swap
-  override def ***[B,C,D,E](f: B => C, g: D => E): ((B,D)) => (C,E) = bd => (f(bd._1), g(bd._2))
-  override def &&&[B,C,D](f: B => C, g: B => D): B => (C,D) = b => ***(f, g)((b,b))
+class Fn1Arrow[B,C](f: B => C) extends Arrow[B, C, Function1] {
+  override def arr = f
+  override def >>>[D](g: C => D) = (b: B) => g(f(b))
+  override def first[D] = bd => (f(bd._1), bd._2)
+  override def second[D] = db => first(db.swap).swap
+  override def ***[D,E](g: D => E) = (bd: (B,D)) => (f(bd._1), g(bd._2))
+  override def &&&[D](g: B => D) = (b: B) => ***(g)((b,b))
 }
 
-object ComplexNums {
-  type Complex = (Int, Int)
-  val show: Complex => String = x => "(" + x._1 + ", " + x._2 + ")"
-  val add: Int => Int => Int = x => y => x + y
-  def getC(): Complex = (7, 3)
-  def twoCs(): Unit => (Complex, Complex) = Unit => (getC(), getC())
+object Arrow {
+  implicit def fn1Arrow[A,B](g: A => B): Fn1Arrow[A, B] = new Fn1Arrow(g)
 }
 
 trait Fn1ArrowDemo {
-  import ComplexNums._
+  import Arrow.fn1Arrow
 
-  val arrow = new Fn1Arrow()
+  val show: Tuple2[Int, Int] => String = x => "(" + x._1 + ", " + x._2 + ")"
+  val add: Int => Int => Int = x => y => x + y
 
-  private[this] val addC: ((Complex, Complex)) => Complex =
-    xy => arrow.***(add(xy._1._1), add(xy._1._2))(xy._2._1, xy._2._2)
+  val fn1ArrowDemo = (add(3) *** add(5)) >>> (add(7) *** add(11)) >>> show >>> println
 
-  val fn1ArrowDemo = arrow.>>>(twoCs(), arrow.>>>(addC, arrow.>>>(show, println)))
-
-  print("fn1ArrowDemo() = ")
-  fn1ArrowDemo()
-}
-
-class InfixFn1Arrow[B,C](f: B => C) {
-  def arr = f
-  def >>>[D](g: C => D) = (b: B) => g(f(b))
-  def first[D](bd: (B,D)) = (f(bd._1), bd._2)
-  def second[D](db: (D,B)) = first(db.swap).swap
-  def ***[D,E](g: D => E) = (bd: (B,D)) => (f(bd._1), g(bd._2))
-  def &&&[D](g: B => D) = (b: B) => ***(g)((b,b))
-}
-
-trait InfixFn1ArrowDemo {
-  import ComplexNums._
-
-  implicit def f1ToArrow[B,C](f: B => C): InfixFn1Arrow[B,C]    = new InfixFn1Arrow(f)
-  implicit def fn0ToArrow[C](f: => C):    InfixFn1Arrow[Unit,C] = new InfixFn1Arrow(Unit => f)
-
-  private[this] val addC: ((Complex, Complex)) => Complex =
-    xy => (add(xy._1._1) *** add(xy._1._2))(xy._2._1, xy._2._2)
-
-  val infixFn1ArrowDemo = (getC(), getC()) >>> addC >>> show >>> println
-
-  print("infixFn1ArrowDemo() = ")
-  infixFn1ArrowDemo()
+  print("fn1ArrowDemo = ")
+  fn1ArrowDemo((1, 2))
 }
 
 // Demo
@@ -305,4 +275,3 @@ object Demo extends App
                with Fn1MonadDemo2
                with StateMonadDemo2
                with Fn1ArrowDemo
-               with InfixFn1ArrowDemo
