93149de58063f7da84c2f51efa84e34f71443437
James Earl Douglas <james@earldouglas.com>
2013-03-25 20:15:26 -0700

    add an an example of a 'is not an enclosing class' error

--- macros/mackrose-example/src/main/scala/mackrose-example.scala
+++ macros/mackrose-example/src/main/scala/mackrose-example.scala
@@ -2,8 +2,11 @@ package com.earldouglas.mackrose
 
 object Example extends App {
 
-  val one = mackrose2(
+  def foo(): String = "bar"
+
+  val one: Int = mackrose2(
     { () =>
+      // foo() // This blows up with 'Example is not an enclosing class`
       println("Holy crap, I'm running at compile time!")
       println("It is now %s.".format(new java.util.Date().toString))
     },
