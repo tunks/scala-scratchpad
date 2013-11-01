ec6844113977f88800fdb71f780caa389fff8d4e
James Earl Douglas <james@earldouglas.com>
2013-10-31 18:06:37 -0700

    implement LogOption in terms of StateT

--- functional-debugging/println.scala
+++ functional-debugging/println.scala
@@ -39,42 +39,85 @@ trait SideEffects {
 
 }
 
-object StateLog {
+trait Functor[F[_],A] {
+  def map[B](f: A => B): F[B]
+}
 
-  def pure[A](a: A): LogOption[A] = new LogOption(log => Some((a, log)))
-  def log(x: String): LogOption[Unit] = new LogOption(log => Some(((), log ++ Seq(x))))
+trait Monad[F[_],A] extends Functor[F,A] {
+  def flatMap[B](f: A => F[B]): F[B]
+}
 
-  class LogOption[A](g: Seq[String] => Option[(A, Seq[String])]) {
-    def run(log: Seq[String]) = g(log)
-    def map[B](f: A => B): LogOption[B] =
-      new LogOption({ l =>
-        g(l) map { x => (f(x._1), x._2) }
-      })
-    def flatMap[B](f: A => LogOption[B]): LogOption[B] =
-      new LogOption({ l =>
-        g(l) flatMap { x => f(x._1).run(x._2) }
-      })
-  }
+object Identity {
 
-  implicit def lift[A](x: Option[A]): LogOption[A] =
-    new LogOption(l => x.map(a => (a, l)))
+  case class ID[A](a: A)
+
+  implicit def id[A](x: ID[A]): Monad[ID,A] =
+    new Monad[ID,A] {
+      def map[B](f: A => B): ID[B] = ID(f(x.a))
+      def flatMap[B](f: A => ID[B]): ID[B] = f(x.a)
+    }
+
+}
+
+object Option {
+
+  implicit def option[A](x: Option[A]): Monad[Option,A] =
+    new Monad[Option,A] {
+      def map[B](f: A => B): Option[B] = x.map(f)
+      def flatMap[B](f: A => Option[B]): Option[B] = x.flatMap(f)
+    }
+
+}
+
+object State {
+
+  import Identity._
+
+  case class StateT[F[_],S,A](run: S => F[(A,S)])
+  type State[A] = StateT[ID,Seq[String],A]
+
+  private type StateTM[F[_],S,A] = Monad[({type λ[α] = StateT[F,S,α]})#λ,A]
+
+  implicit def stateT[F[_],S,A](x: StateT[F,S,A])
+      (implicit lift: F[(A,S)] => Monad[F,(A,S)]): StateTM[F,S,A] =
+    new StateTM[F,S,A] {
+      def run(s: S): F[(A,S)] = x.run(s)
+      def map[B](f: A => B): StateT[F,S,B] =
+        new StateT[F,S,B]({ l => x.run(l) map { x => (f(x._1), x._2) } })
+      def flatMap[B](f: A => StateT[F,S,B]): StateT[F,S,B] =
+        new StateT[F,S,B]({ l => x.run(l) flatMap { x => f(x._1).run(x._2) } })
+    }
+
+}
+
+object Log {
+
+  import State._
+
+  type LogT[F[_],A] = StateT[F,Seq[String],A]
+  def logT[F[_],A](x: F[A])(implicit lift: F[A] => Monad[F,A]): LogT[F,A] =
+    new LogT(l => x.map(a => (a, l)))
+
+  def log(x: String): LogT[Option,Unit] = new LogT(log => Some(((), log ++ Seq(x))))
 
 }
 
 trait NoSideEffects {
 
   import Arithmetic._
-  import StateLog._
+  import Option._
+  import State._
+  import Log._
 
   lazy val resultM =
     for {
-      x1 <- lift(parse("42"))
+      x1 <- logT(parse("42"))
       _  <- log("x1: " + x1)
       x2  = mult(x1)(2)
       _  <- log("x2: " + x2)
       x3  = add(x2)(42)
       _  <- log("x3: " + x3)
-      x4 <- lift(div(x3)(3))
+      x4 <- logT(div(x3)(3))
       _  <- log("x4: " + x4)
     } yield x4
 
